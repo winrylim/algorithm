64947480f67db830df25c807208cba7d8bfb4cf6
winry.lim <winry.lim@gmail.com>
2021-10-02 11:46:17 +0900

    Update Greedy Algorithm

--- Greedy Algorithm/TEST.py
+++ Greedy Algorithm/TEST.py
@@ -3,7 +3,7 @@ count = 0
 conin_types = [500, 100, 50, 10]
 
 for coin in conin_types:
-    count += (int)(n / coin)
+    count += n // coin # n // coin -> (int)(n / coin)
     n %= coin
 
 print(count)
