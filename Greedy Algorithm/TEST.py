1400cef40ca901ad6aeb22b30c50aba15d6a88d5
winry.lim <winry.lim@lge.com>
2021-10-08 18:08:43 +0900

    Greedy Algorithm

--- Greedy Algorithm/TEST.py
+++ Greedy Algorithm/TEST.py
@@ -8,24 +8,99 @@
 
 # print(count)
 
-n, m, k = map(int, input().split())
-data = list(map(int, input().split()))
+# n, m, k = map(int, input().split())
+# data = list(map(int, input().split()))
 
-data.sort() #오름차순으로 정렬
-first = data[n-1]
-second = data[n-2]
+# data.sort() #오름차순으로 정렬
+# first = data[n-1]
+# second = data[n-2]
+
+# result = 0
+
+
+# while True:
+#     for i in range(k):
+#         if m == 0:
+#             break
+#         result += first
+#         m -=1
+#     if m == 0:
+#         break
+#     result += second
+#     m -=1
+
+# count = int(m/(k+1)) * k
+# count += m%(k+1)
+
+# result += count * first
+# result += (m-count) * second
+
+# print(result)
+
+# n, m = map(int, input().split())
+
+# result = 0
+
+# for i in range(n):
+#     data = list(map(int, input().split()))
+#     min_value = min(data)
+#     result = max(result, min_value)
+
+# print(result)
+
+# for i in range(n):
+#     data = list(map(int, input().split()))
+
+#     min_value = 10001 #max
+
+#     for a in data:
+#         min_value = min(min_value, a)
+
+#     result = max(min_value, result)
+
+# print(result)
+
+n, k = map(int, input().split())
 
 result = 0
 
+# while n >= k:
+#     while n % k != 0:
+#         n -= 1
+#         result += 1
+#     n //= k
+#     result += 1
+
+# while n > 1:
+#     n-=1
+#     result += 1
+
+# while True:
+#     target = (n//k) * k
+#     result += (n - target)
+
+#     n = target
+
+#     if n < k:
+#         break
+
+#     result += 1
+#     n //= k
+
+# result += (n-1)
+
+# print(result)
+
 while True:
-    for i in range(k):
-        if m == 0:
-            break
-        result += first
-        m -=1
-    if m == 0:
+    target = (n%k)
+    result += target
+    n -= target
+
+    if n < k:
+        result += (n-1)
         break
-    result += second
-    m -=1
+
+    result += 1
+    n //= k
 
 print(result)
